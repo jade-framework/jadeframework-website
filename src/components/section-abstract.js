21d0164bfcbf26e500a7e615764acb3799515a38
edmondtam1 <edmondtam2@gmail.com>
2020-09-11 08:45:30 +0800

    Minor fixes

--- src/components/section-abstract.js
+++ src/components/section-abstract.js
@@ -9,7 +9,7 @@ const SectionAbstract = ({ num, title, id }) => {
           Jade is an open-source framework that makes it simple to deploy and
           maintain JAMstack applications on AWS cloud infrastructure. The
           JAMstack is a web development architecture that utilizes modern tools
-          and practices to make web apps fast, secure and highly scalable. This
+          and practices to make web apps fast, secure, and highly scalable. This
           case study examines how Jade abstracts away the time and complexity of
           writing backend code related to the underlying infrastructure,
           allowing developers to focus on building their applications.
