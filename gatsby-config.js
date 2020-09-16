14edebb91d96895430c818269a54dae55c90c96c
Jeremy Crichton <22128243+JeremyCrichton@users.noreply.github.com>
2020-09-15 18:47:29 -0700

    update site image

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `Jade Framework`,
     description: `A framework for deploying JAMstack applications.`,
     author: `Jade Framework`,
-    image: "/jade_logo-type.png",
+    image: "/jade_logo-full-square.png",
     siteUrl: `https://jadeframework.dev`,
   },
   plugins: [
